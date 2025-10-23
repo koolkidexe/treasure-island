a30f6bbbb6e93ff571c88f777e35b4624da0feb6
koolkidexe <103842906+koolkidexe@users.noreply.github.com>
2025-10-23 17:10:37 -0400

    Update islandsofdiscovery.py

--- islandsofdiscovery.py
+++ islandsofdiscovery.py
@@ -74,7 +74,7 @@ def check_end():
     # Reveal coordinates if score >= 100, only once
     if st.session_state.score >= 100 and not st.session_state.revealed_coords:
         st.session_state.message += (
-            f"\n📍🏆 You won! You got {st.session_state.score} points.\n"
+            f"\n You won! You got {st.session_state.score} points.\n"
             f"📍 Here are the coordinates: (32N, 48E)"
         )
         st.session_state.revealed_coords = True
