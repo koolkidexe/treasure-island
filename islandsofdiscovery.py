2fe16b8013624b1dff4c207141868e6257d5ad4c
koolkidexe <103842906+koolkidexe@users.noreply.github.com>
2025-12-09 20:39:45 -0500

    Update islandsofdiscovery.py

--- islandsofdiscovery.py
+++ islandsofdiscovery.py
@@ -78,7 +78,7 @@ def check_end():
         st.session_state.message += (
             f"<br><div style='background-color: #ebf9ed; color: #178237; padding: 10px; border-radius: 4px;'>"
             f"🏆 You won! You got **{st.session_state.score} points**.<br>"
-            f"📍 Here are the coordinates: **(32N, 48E)**"
+            f"📍 Here are the coordinates: **(41°43'25" N, 72°34'5" W)**"
             f"</div>"
         )
         st.session_state.revealed_coords = True
